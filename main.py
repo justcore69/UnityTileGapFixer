c1a146675bc2553ab28b15f353ff4168d6879a20
Pieceofsoil <109795959+Pieceofsoil@users.noreply.github.com>
2023-07-16 21:49:04 +0300

    Transparent tiles checking

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from PIL import Image
 
 def paste_images(background, paste, x, y):
@@ -8,25 +9,25 @@ def paste_images(background, paste, x, y):
 
     return new_image
 
-# Open the image
+# Open the tileset input image
 image = Image.open("input.png")
 
-# Define the tile size and spacing
 tile_size = 16
 spacing = 2
+total_tile_size = tile_size + spacing
 
-# Calculate the number of tiles horizontally and vertically
 num_tiles_horizontal = image.width // tile_size
 num_tiles_vertical = image.height // tile_size
 
-# Calculate the new image size with spacing
+print(num_tiles_vertical)
+print(num_tiles_horizontal)
+
 new_width = (num_tiles_horizontal * (tile_size + spacing)) + spacing
 new_height = (num_tiles_vertical * (tile_size + spacing)) + spacing
 
-# Create a new image with the required size and RGBA mode
 new_image = Image.new("RGBA", (new_width, new_height))
 
-# Iterate over the tiles and move them with spacing
+
 for i in range(num_tiles_horizontal):
     for j in range(num_tiles_vertical):
         x = i * tile_size
@@ -36,23 +37,55 @@ for i in range(num_tiles_horizontal):
         new_y = (j * (tile_size + spacing)) + spacing
         new_image.paste(tile, (new_x, new_y))
 
+
 ox, oy = new_image.size
-outline_image = new_image.crop((2, 2, ox-2, oy-2))
+outline_image = new_image.crop((spacing, spacing, ox-spacing, oy-spacing))
+
+
+outline_image.save("outline.png")
+tiles_with_empty_pixels = []
+
+if not os.path.exists("tiles"):
+    os.makedirs("tiles")
+
+# Iterate over each tile
+for j in range(num_tiles_vertical):
+    for i in range(num_tiles_horizontal):
+        x = i * total_tile_size
+        y = j * total_tile_size
+
+        tile = outline_image.crop((x, y, x + tile_size, y + tile_size))
+        tile.save(f"tiles/tile_{int(x/total_tile_size)}_{int(y/total_tile_size)}.png")
+
+        # Check if the tile has any empty pixels
+        empty_pixel_found = False
+        for pixel in tile.getdata():
+            if pixel[3] == 0: 
+                empty_pixel_found = True
+                break
+
+        if empty_pixel_found:
+            tiles_with_empty_pixels.append((i, j))
+
+
+print("Tiles with empty pixels:")
+print(tiles_with_empty_pixels)
 
 output_image = Image.new("RGBA", new_image.size, (0, 0, 0, 0))
 
-output_image = paste_images(output_image, outline_image, 1, 1)#up left 
-output_image = paste_images(output_image, outline_image, 3, 1)#up right
-output_image = paste_images(output_image, outline_image, 3, 3)#down right
-output_image = paste_images(output_image, outline_image, 1, 3)#down left
+# Outline adding
 
-output_image = paste_images(output_image, outline_image, 2, 1)#up
-output_image = paste_images(output_image, outline_image, 2, 3)#down
-output_image = paste_images(output_image, outline_image, 1, 2)#left
-output_image = paste_images(output_image, outline_image, 3, 2)#right
+output_image = paste_images(output_image, outline_image, 1, 1)# up left 
+output_image = paste_images(output_image, outline_image, 3, 1)# up right
+output_image = paste_images(output_image, outline_image, 3, 3)# down right
+output_image = paste_images(output_image, outline_image, 1, 3)# down left
+
+output_image = paste_images(output_image, outline_image, 2, 1)# up
+output_image = paste_images(output_image, outline_image, 2, 3)# down
+output_image = paste_images(output_image, outline_image, 1, 2)# left
+output_image = paste_images(output_image, outline_image, 3, 2)# right
+
 
 output_image = paste_images(output_image, new_image, 0, 0)
-
-#TODO: slope mask must be a rectangle
 
 output_image.save("output1.png")
